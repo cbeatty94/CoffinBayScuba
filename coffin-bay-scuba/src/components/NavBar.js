7d8705782b5f30d7c7341c7b5c221b83a5afb74b
Burke <beatty.burke@gmail.com>
2021-12-16 12:16:35 -0800

    added new logo file and styling to navbar

--- coffin-bay-scuba/src/components/NavBar.js
+++ coffin-bay-scuba/src/components/NavBar.js
@@ -3,18 +3,20 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './NavBar.css'
+import newSealLogo from '../logo/newSealLogo.png'
+// import Form from 'react-bootstrap/Form'
+// import FormControl from 'react-bootstrap/FormControl'
 // import '../App.css'
 
 
 
 function NavBar() {
     return(
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar className='navbar' expand="lg">
+        <img href="/" src={newSealLogo} alt='Seal Logo' className='navLogo'  />
         <Container fluid>
-            <Navbar.Brand href="/">Coffin Bay Scuba</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -22,28 +24,27 @@ function NavBar() {
                 style={{ maxHeight: '100px' }}
                 navbarScroll
             >
-                <Nav.Link href="#chart">Courses</Nav.Link>
-                <Nav.Link href="#action2">Dive Club</Nav.Link>
-                <NavDropdown title="More Info" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">About Us</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Contact Us</NavDropdown.Item>
-                <NavDropdown.Item href="#action5">FAQ</NavDropdown.Item>
-                <NavDropdown.Item href="#action6">Login</NavDropdown.Item>
+                <Nav.Link href="#chart" className='navLink'>Courses</Nav.Link>
+                <Nav.Link href="#action2" className='navLink'>Dive Club</Nav.Link>
+                <NavDropdown title="More Info" className='navLink' id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3" className='navLink'>About Us</NavDropdown.Item>
+                <NavDropdown.Item href="#action4" className='navLink'>Contact Us</NavDropdown.Item>
+                <NavDropdown.Item href="#action5" className='navLink'>FAQ</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action7">
+                <NavDropdown.Item href="#action7" className='navLink'>
                     Photos
                 </NavDropdown.Item>
                 </NavDropdown>
             </Nav>
-            <Form className="d-flex">
+            {/* <Form className="d-flex">
                 <FormControl
                 type="search"
                 placeholder="Search our Site!"
                 className="me-2"
                 aria-label="Search"
-                />
-                <Button variant="success">Search</Button>
-            </Form>
+                /> */}
+            {/* </Form> */}
+            <Button className='navButton'>Sign Up/Register</Button>
             </Navbar.Collapse>
         </Container>
         </Navbar>
